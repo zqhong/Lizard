e5dfc2faad123a0cc797e8cab39c3db6ebcaa9a2
zqhong <i@zqhong.com>
2016-09-18 08:26:23 +0800

    Support i18n for the part of forum

---
+++ resources/lang/zh-CN/forum.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of Lizard.
+ *
+ * @author zqhong <i@zqhong.com>
+ * @date 2016/9/12 22:39
+ * @filename thread.php
+ * @encoding UTF-8
+ */
+
+
+return [
+    'homepage' => '首页',
+    'threads' => '话题',
+    'login' => '登陆',
+    'logout' => '退出登录',
+    'welcome' => '你好，:username',
+    'required_login' => '您还未登录。',
+];
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,9 +4,9 @@
     <div class="row">
         <div class="col-md-9">
             <div class="panel panel-default">
-                <div class="panel-heading">Homepage</div>
+                <div class="panel-heading">{{ trans('forum.homepage') }}</div>
                 <div class="panel-body">
-                    <h2>Threads</h2>
+                    <h2>{{ trans('forum.threads') }}</h2>
                     @foreach($threads as $thread)
                         <h3>{!! link_to_route('thread.show', $thread->title, [$thread->id]) !!}</h3>
                         <p>
@@ -31,10 +31,11 @@
                 <div class="panel-heading">Lizard</div>
                 <div class="panel-body">
                     @if(Auth::guest())
-                        You're not login. Please {{ link_to_route('login', 'Login') }}
+                        {{ trans('forum.required_login') }}
+                        {{ link_to_route('login', trans('forum.login')) }}
                     @else
-                        Hello, {{ Auth::user()->username }}
-                        {{ link_to_route('logout', "Logout") }}
+                        {{ trans('forum.welcome', ['username' => Auth::user()->username]) }}
+                        {{ link_to_route('logout', trans('forum.logout')) }}
                     @endif
                 </div>
             </div>
