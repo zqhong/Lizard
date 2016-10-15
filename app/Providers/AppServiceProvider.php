55c76db6aa8ea33e954202ca10ba83330f6e4cbc
zqhong <i@zqhong.com>
2016-10-15 13:33:26 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace Lizard\Providers;
 use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
 use Illuminate\Support\ServiceProvider;
 use Laracasts\Generators\GeneratorsServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() == 'local') {
             $this->app->register(GeneratorsServiceProvider::class);
             $this->app->register(DebugbarServiceProvider::class);
+            $this->app->register(IdeHelperServiceProvider::class);
         }
     }
 }
